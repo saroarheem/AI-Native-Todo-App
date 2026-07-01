d372ceb3f75cc8d0a00ae6041f9805308331c186
saroarheem <saroarheem@gmail.com>
2026-07-02 05:42:22 +0600

    page-not-found

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { CreateTodo } from './create-todo/create-todo';
 import { TodoList } from './todo-list/todo-list';
 import { EditTodo } from './edit-todo/edit-todo';
+import { PageNotFound } from './page-not-found/page-not-found';
 
 export const routes: Routes = [
     {path: "list", component: TodoList},
     {path: "create", component: CreateTodo},
     {path: "todo/:id", component: EditTodo},
-    {path: "", redirectTo:"list", pathMatch: "full"}
+    {path: "", redirectTo:"list", pathMatch: "full"},
+    {path: "**", component: PageNotFound},
 ];
